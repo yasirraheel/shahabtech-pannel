54f6c7ec2f2fd44bbf5b66ac3d3739796eca3824
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-06-21 06:53:43 +0500

    Fix Javascript references for Quick Generate and Copy features

--- core/resources/views/admin/users/create.blade.php
+++ core/resources/views/admin/users/create.blade.php
@@ -48,7 +48,7 @@
                                     <div class="input-group">
                                         <input class="form-control" type="password" name="password" id="passwordField" required>
                                         <button type="button" class="input-group-text" id="togglePassword"><i class="las la-eye"></i></button>
-                                        <button type="button" class="input-group-text" onclick="copyPassword()"><i class="las la-copy"></i></button>
+                                        <button type="button" class="input-group-text copy-btn"><i class="las la-copy"></i></button>
                                     </div>
                                     <small><a href="javascript:void(0)" id="generatePasswordBtn">@lang('Generate Random Password')</a></small>
                                 </div>
@@ -114,15 +114,107 @@
 @endsection
 
 @push('script')
-    <script>
-        (function ($) {
-            "use strict";
-            $('select[name=country]').on('change', function () {
-                $('input[name=mobile_code]').val($('select[name=country] :selected').data('mobile_code'));
-                $('.mobile-code').text('+' + $('select[name=country] :selected').data('mobile_code'));
-            });
+<script>
+    (function ($) {
+        "use strict";
+        
+        $('select[name=country]').on('change', function () {
             $('input[name=mobile_code]').val($('select[name=country] :selected').data('mobile_code'));
             $('.mobile-code').text('+' + $('select[name=country] :selected').data('mobile_code'));
-        })(jQuery);
-    </script>
+        });
+        $('input[name=mobile_code]').val($('select[name=country] :selected').data('mobile_code'));
+        $('.mobile-code').text('+' + $('select[name=country] :selected').data('mobile_code'));
+
+        // Auto-fill email based on username
+        $('input[name="username"]').on('input', function() {
+            let val = $(this).val();
+            if(val) {
+                $('input[name="email"]').val(val.toLowerCase() + '@topdealsplus.com');
+            } else {
+                $('input[name="email"]').val('');
+            }
+        });
+
+        // Toggle password visibility
+        $('#togglePassword').on('click', function() {
+            let pwd = $('#passwordField');
+            if(pwd.attr('type') === 'password') {
+                pwd.attr('type', 'text');
+                $(this).html('<i class="las la-eye-slash"></i>');
+            } else {
+                pwd.attr('type', 'password');
+                $(this).html('<i class="las la-eye"></i>');
+            }
+        });
+
+        // Copy Password
+        $('.copy-btn').on('click', function () {
+            let copyText = document.getElementById("passwordField");
+            if(copyText.type === "password") {
+                copyText.type = "text";
+                copyText.select();
+                document.execCommand("copy");
+                copyText.type = "password";
+            } else {
+                copyText.select();
+                document.execCommand("copy");
+            }
+            notify('success', 'Password Copied!');
+        });
+
+        function generateRandomString(length) {
+            let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+            let str = "";
+            for (let i = 0; i < length; i++) {
+                str += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+            return str;
+        }
+        
+        function generateRandomNumberString(length) {
+            let chars = "0123456789";
+            let str = "";
+            for (let i = 0; i < length; i++) {
+                str += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+            return str;
+        }
+
+        // Generate Random Password
+        $('#generatePasswordBtn').on('click', function() {
+            $('#passwordField').val(generateRandomString(12));
+            $('#passwordField').attr('type', 'text');
+            $('#togglePassword').html('<i class="las la-eye-slash"></i>');
+        });
+
+        // Quick Generate User
+        $('#generateUserBtn').on('click', function() {
+            let firstNames = ["John", "Emma", "Michael", "Sarah", "David", "Jessica", "James", "Emily", "Robert", "Olivia", "William", "Sophia", "Daniel", "Isabella"];
+            let lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez"];
+            
+            let fName = firstNames[Math.floor(Math.random() * firstNames.length)];
+            let lName = lastNames[Math.floor(Math.random() * lastNames.length)];
+            let rNum = Math.floor(Math.random() * 9000) + 1000;
+            
+            $('input[name="firstname"]').val(fName);
+            $('input[name="lastname"]').val(lName);
+            
+            let uName = fName.toLowerCase() + '_' + rNum;
+            $('input[name="username"]').val(uName).trigger('input'); // This triggers the email auto-fill
+            
+            $('#generatePasswordBtn').click();
+            
+            // Random unique mobile number (10 digits)
+            $('input[name="mobile"]').val(generateRandomNumberString(10));
+            
+            // Randomly select a country
+            let options = $('#country option');
+            let randomOption = options[Math.floor(Math.random() * options.length)];
+            $('#country').val(randomOption.value).trigger('change');
+            
+            notify('success', 'Random user data generated successfully!');
+        });
+        
+    })(jQuery);
+</script>
 @endpush
