054ec95f42df53e8ff4aa23f1bd9ef9b1089233a
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-08-06 13:39:49 +0500

    Cleanup temporary debug scripts

--- core/check_group27.php
+++
@@ -1,8 +0,0 @@
-<?php
-$omnireachPath = '/home/u559276167/domains/shahabtech.com/public_html/omnireach/src';
-require $omnireachPath . '/vendor/autoload.php';
-$app = require $omnireachPath . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$group = \DB::table('contact_groups')->where('id', 27)->first();
-echo "GROUP 27: " . json_encode($group) . "\n";
--- core/set_wa_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$g = gs();
-$g->admin_whatsapp = ["923006859611"];
-$g->save();
-echo "ADMIN WHATSAPP CONFIGURED: " . json_encode($g->admin_whatsapp) . "\n";
