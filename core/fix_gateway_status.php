e0e6a873c893ad3ed38f75e9f82aeffff55592db
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-08-06 11:42:35 +0500

    Update fix_gateway_status.php

--- core/fix_gateway_status.php
+++ core/fix_gateway_status.php
@@ -4,9 +4,7 @@ require $omnireachPath . '/vendor/autoload.php';
 $app = require $omnireachPath . '/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-// Check Status::ACTIVE->value
-echo "STATUS ACTIVE VALUE: " . App\Enums\Status::ACTIVE->value . "\n";
+echo "STATUS ACTIVE VALUE: " . App\Enums\Common\Status::ACTIVE->value . "\n";
 
-// Update status = 1 for active gateways
-\DB::table('gateways')->whereIn('id', [5, 8])->update(['status' => App\Enums\Status::ACTIVE->value]);
-echo "GATEWAY STATUSES UPDATED TO ACTIVE VALUE!\n";
+\DB::table('gateways')->whereIn('id', [5, 8])->update(['status' => App\Enums\Common\Status::ACTIVE->value]);
+echo "GATEWAY STATUSES UPDATED TO '1'!\n";
