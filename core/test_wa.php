fd5bd4b6a80c63a394ec1f9f5c9b5e90c4573b79
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2026-08-06 11:27:53 +0500

    Update test_wa.php debug headers

--- core/test_wa.php
+++ core/test_wa.php
@@ -13,14 +13,20 @@ $data = [
 curl_setopt_array($curl, [
     CURLOPT_URL => "https://omnireach.shahabtech.com/api/whatsapp/send",
     CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => json_encode($data),
     CURLOPT_HTTPHEADER => [
         "Api-key: e637cd7e-c2bb-406f-ad30-8ae69178e1f6",
-        "Content-Type: application/json"
+        "Content-Type: application/json",
+        "Accept: application/json"
     ],
 ]);
 
 $response = curl_exec($curl);
+$info = curl_getinfo($curl);
 curl_close($curl);
+
+echo "HTTP CODE: " . $info['http_code'] . "\n";
+echo "EFFECTIVE URL: " . $info['url'] . "\n";
 echo "RESPONSE: " . $response . "\n";
